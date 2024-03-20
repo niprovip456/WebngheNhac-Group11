828297b670dd8729002a1a4f56e6060b98924be1
niprovip456 <niprovip4852@gmail.com>
2024-03-20 17:30:52 +0700

    Nộp giao diện trang web (HTML)

---
+++ README.md
@@ -0,0 +1,22 @@
+Web nghe nhạc Nhóm 11 
+## Giao Diện
+
+-  Giao diện được lấy ý tưởng từ web Zingmp3,  youtube music
+-  Đa số data được gọi từ Api
+-  Nguồn tham khảo <https://www.npmjs.com/package/zingmp3-api-full>
+-  Nguồn dữ liệu api được thu nhập trên các mxh:
+
+
+
+## Chức Năng hiện tại
+
+      -  Play / Pause
+      -  Next / Prev
+      -  Next bài ngẫu nhiên
+      -  Lặp lại bài hát
+      -  Click thanh progress tua nhạc
+      -  Click và kéo On / Off Volume
+      -  Đăng nhâp , Đăng ký 
+
+
+
---
+++ src/app/stores.js
@@ -0,0 +1,28 @@
+import { configureStore } from "@reduxjs/toolkit"
+import logged from "../features/Logged/loggedFeatures"
+import Lyrics from "../features/Lyrics/Lyrics"
+import setTextBtn from "../features/MvState/MvStateFeatures"
+import toggleOpenMain from "../features/openMainFull/openMainFullFeatures"
+import queueNowPlay from "../features/QueueFeatures/QueueFeatures"
+import currentTimes from "../features/QueueFeatures/SetTimeCurrent"
+import themeToggle from "../features/setTheme/themeSetFeatures"
+import setting from "../features/SettingPlay/settingPlay"
+import setOpenMainMv from "../features/ToggleMainMv/toggleMainMv"
+import toggleRight from "../features/toggleRight/toggleRight"
+import users from "../features/User/userFeatures"
+
+export const store = configureStore({
+   reducer: {
+      themetoggle: themeToggle,
+      toggleright: toggleRight,
+      setTextBtn: setTextBtn,
+      setOpenMainMv: setOpenMainMv,
+      toggleOpenMain: toggleOpenMain,
+      queueNowPlay: queueNowPlay,
+      logged: logged,
+      setting: setting,
+      lyrics: Lyrics,
+      currentTimes: currentTimes,
+      users: users,
+   },
+})
